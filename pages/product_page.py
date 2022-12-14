a40a357a5547778479ff61843d7caa252c392f09
VitaliyStyranko <vit77st@yahoo.com>
2022-12-14 18:38:24 -0500

    CureSkin-Automation-project

---
+++ pages/product_page.py
@@ -0,0 +1,41 @@
+from selenium.webdriver.common.by import By
+from pages.base_page import Page
+from selenium.webdriver.common.action_chains import ActionChains
+from time import sleep
+
+
+class ProductPage(Page):
+
+    ADD_TO_CART_BTN = (By.CSS_SELECTOR, ".button[type='submit'], [name='add']")
+    CONF_SHOWN = (By.XPATH, "//div[@class='cart-notification focus-inset animate active']"
+                            "//*[contains(text(), 'View my cart (1)')]")
+    VIEW_MY_CART_BTN = (By.ID, 'cart-notification-button')
+    PRODUCT_TITLE = (By.XPATH, '//a[@class="cart-item__name h4 break"]')
+
+    def click_add_to_cart(self):
+        self.click(*self.ADD_TO_CART_BTN)
+
+    def verify_confirmation(self):
+        self.wait_for_element_appear(*self.CONF_SHOWN)
+
+    def click_view_btn(self):
+        self.click(*self.VIEW_MY_CART_BTN)
+
+    def verify_item(self, expected_result):
+        actual_result = self.driver.find_element(*self.PRODUCT_TITLE).text
+        assert expected_result == actual_result, f'Error! Actual text {actual_result} does not match expected{expected_result}'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
