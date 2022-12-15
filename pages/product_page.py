7ad172fb08e2a3b705a82aa255e906abda403083
VitaliyStyranko <vit77st@yahoo.com>
2022-12-15 18:06:08 -0500

    CureSkin-Automation-project

--- pages/product_page.py
+++ pages/product_page.py
@@ -9,7 +9,7 @@ class ProductPage(Page):
     ADD_TO_CART_BTN = (By.CSS_SELECTOR, ".button[type='submit'], [name='add']")
     CONF_SHOWN = (By.XPATH, "//div[@class='cart-notification focus-inset animate active']"
                             "//h2[contains(text(), 'added to your cart')]")
-    VIEW_MY_CART_BTN = (By.ID, 'cart-notification-button')
+    VIEW_MY_CART_BTN = (By.CSS_SELECTOR, "a#cart-notification-button")
     PRODUCT_TITLE = (By.XPATH, '//a[@class="cart-item__name h4 break"]')
 
     def click_add_to_cart(self):
