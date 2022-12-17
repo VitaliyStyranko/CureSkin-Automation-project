d538f4b2475eaaadc7856117cba3cbb550836f23
VitaliyStyranko <vit77st@yahoo.com>
2022-12-16 20:07:21 -0500

    CureSkin-Automation-project

--- app/application.py
+++ app/application.py
@@ -1,5 +1,6 @@
 from pages.main_page import MainPage
 from pages.product_page import ProductPage
+from pages.collections_page import CollectionsPage
 
 
 class Application:
@@ -8,3 +9,5 @@ class Application:
         self.driver = driver
         self.main_page = MainPage(self.driver)
         self.product_page = ProductPage(self.driver)
+        self.collections_page = CollectionsPage(self.driver)
+
